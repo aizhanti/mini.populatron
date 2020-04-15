34c60352d1b5f33df5cb2432ce1a1e7a0f3d2811
aizhanti <a.t.imankulova@gmail.com>
2020-04-16 01:34:45 +0900

    put everything into module.exports
    Co-authored-by: cwcraw <cwcraw@users.noreply.github.com>

--- populatron.js
+++ populatron.js
@@ -1,102 +1,18 @@
 const fs = require("fs");
 const neatCsv = require("neat-csv");
 
-// fs.readFile('./cities.csv', async (err, data) => {
-//   if (err) {
-//     console.error(err, '1')
-//     return
-//   }
-//   let neatData = (await neatCsv(data))
-
-// })
-
-// let totalPopulation = (input) => {
-//   let output = 0
-//   for (const el of input) {
-//     output = output + Number(el['population'])
-//   }
-//   // console.log(output)
-//   return output
-// }
-
-// totalPopulation(neatData)
-
-fs.readFile("./cities.csv", "utf8", async (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  let totalPopulation = (input) => {
-    console.log(input);
-    let output = 0;
-    output = input.reduce((acc, curr) => {
-      return Number(acc) + Number(curr["population"]);
-    }, 0);
-    console.log(output);
-    return output;
-  };
-  let neatData = await neatCsv(data);
-  return totalPopulation(neatData);
-});
-
-// let tp = (input) => {
-//   let output = 0
-//   output = input.reduce((acc, curr, 0) => { return acc + Number(curr['population']) })
-//   // for (const el of input) {
-//   //   output = output + Number(el['population'])
-//   // }
-//   console.log(output)
-//   return output
-// }
-
-// function totalPopulation(tp) {
-//   fs.readFile('./cities.csv', async (err, data) => {
-//     if (err) {
-//       console.error(err, '1')
-//       return
-//     }
-//     let neatData = (await neatCsv(data))
-//   })
-// }
-
-// const promise = (new Promise(() => {
-//   fs.readFile('./cities.csv', async (err, data) => {
-//     if (err) {
-//       console.error(err)
-//       return
-//     }
-//     let neatData = (await neatCsv(data))
-//     let totalPopulation = (input) => {
-//       let output = 0
-//       for (el of input) {
-//         output = output + Number(el['population'])
-//       }
-//       console.log(output)
-//       return output
-//     }
-//     // totalPopulation(neatData)
-//   })
-// })).finished.then(totalPopulation(neatData), () => { console.log('err') })
-
-// // fs.readFile('./cities.csv', async (err, data) => {
-// //   if (err) {
-// //     console.error(err)
-// //     return
-// //   }
-// //   // let neatData = (await neatCsv(data))
-// //   // totalPopulation(neatData)
-// // })
-
-// // totalPopulation(input)
-// // let totalPopulation = (input) => {
-// //   let output = 0
-// //   for (el of input) {
-// //     output = output + Number(el['population'])
-// //   }
-// //   console.log(output)
-// //   return output
-// // }
-
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    let output = 0;
+    fs.readFile("./cities.csv", async (err, data) => {
+      if (err) {
+        return;
+      }
+      let neatData = await neatCsv(data);
+      output = neatData.reduce((acc, curr) => {
+        return Number(acc) + Number(curr["population"]);
+      }, 0);
+      onFinished(output);
+    });
+  },
 };
